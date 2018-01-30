4b172081210aa13eef57aaf97ae9ef49bf80ec23
ohouens <rohouens@yahoo.fr>
2018-01-30 18:21:37 +0100

    Add files via upload

---
+++ Robot.py
@@ -0,0 +1,16 @@
+class Robot:
+	"""Classe caractérisé par:
+		Ses coordonnées
+		Sa direction
+		Sa dimension
+		Sa vitesse"""
+
+	def __init__(self, position, direction, dimension, vitesse):
+		self.position = position
+		self.direction = direction
+		self.dimension = dimension
+		self.vitesse = 0
+
+	
+
+	
