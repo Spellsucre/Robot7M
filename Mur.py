1f8fd7d1f85b69270a57bcc6d00178b189d34b9b
FabienMnsn <35796179+FabienMnsn@users.noreply.github.com>
2018-01-31 19:55:21 +0100

    Update Mur.py
    Retouche du constructeur et ajout d'une fonction d'affichage d'un Mur

--- Mur.py
+++ Mur.py
@@ -6,17 +6,18 @@ class Mur(Cube):
         -sa largueur
         -sa longueur"""
 
-    def __init__(self, x, y, z, larg, long, haut):
+ def __init__(self, x, y, z, larg, long, haut):
         """Constructeur de la classe Mur"""
-        self.x = x
-        self.y = y
-        self.z = z
-        self.long = long
-        self.larg=larg
-        self.haut=haut
+        Cube.__init__(self,x,y,z,larg,long,haut)
+
+    def safficher(self):
+        """Methode d'affichage d'un mur au format :
+        mur[x= , y= , z= , larg= , long= , haut= ]
+        """
+        print("Mur(x=%.2f,y=%.2f,z=%.2f, larg=%.2f,long=%.2f,haut=%.2f)"%(self.x, self.y, self.z, self.larg, self.long, self.haut))
 
 
-def Creation_Mur(x,y,z,long,arene):
+def Creation_Mur(x,y,z,long,arene):     #ajouter des parametres c'est étrange pour une création rapide de mur mais bon pourquoi pas :)
     """Création d'un mur avec une hauteur et une epaisseur fixé par les limites de l'Arene"""
     
     return Mur(x, y, z, 10, long, arene.lz)
