292a6fecd1005ab99dd0fb695c69c07655c019c1
FabienMnsn <35796179+FabienMnsn@users.noreply.github.com>
2018-02-03 17:39:52 +0100

    Update Sol.py

--- Sol.py
+++ Sol.py
@@ -1,4 +1,6 @@
-from Cube import Cube
+from Cube import *
+from Arene import *
+
 class Sol(Cube):
     """Classe héritant de la classe Cube, caractérisée par:
         -ses coordonnées: x, y, z
@@ -7,16 +9,23 @@ class Sol(Cube):
         -sa longueur"""
 
     def __init__(self, x, y, z, larg, long):
-        """Constructeur de la classe Mur"""
-        Cube.__init__(self,x,y,z,larg,long,0)
+        """Constructeur de la classe Cube"""
+        Cube.__init__(self,x,y,z,larg,long,1)
 
     def safficher(self):
-        """Methode d'affichage d'un mur au format :
-        mur[x= , y= , z= , larg= , long= , haut= ]
+        """Methode d'affichage d'un sol au format :
+        sol[x= , y= , z= , larg= , long= , haut= ]
         """
         print("Sol(x=%.2f,y=%.2f,z=%.2f, larg=%.2f,long=%.2f,haut=%.2f)"%(self.x, self.y, self.z, self.larg, self.long, self.haut))
 
 
-def Creation_Sol(x,y,z,long,arene):     #ajouter des parametres c'est étrange pour une création rapide de mur mais bon pourquoi pas :)
-    """Création d'un mur avec une hauteur et une epaisseur fixé par les limites de l'Arene"""
-    return Sol(x, y, z, 10, long)
+def Creation_Sol(arene): #parametre obligatoire pour pouvoir recupérer les dimensions de l'arene
+    """Création d'un sol avec une hauteur = 1 et une taille (larg, long) par les limites de l'Arene"""
+    x = 0
+    y = 0
+    z = 0
+
+    larg = arene.lx
+    long = arene.ly
+
+    return Sol(x, y, z, larg, long)
