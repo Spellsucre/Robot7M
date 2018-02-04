8780e0ce49899a4ff028317a1dd6d4fcd91fdce1
FabienMnsn <35796179+FabienMnsn@users.noreply.github.com>
2018-02-04 20:14:46 +0100

    Update Mur.py
    modification du constructeur automatique Creation_Mur(arene)

--- Mur.py
+++ Mur.py
@@ -1,4 +1,6 @@
 from Cube import Cube
+import random
+
 class Mur(Cube):
     """Classe héritant de la classe Cube, caractérisée par:
         -ses coordonnées: x, y, z
@@ -6,7 +8,7 @@ class Mur(Cube):
         -sa largueur
         -sa longueur"""
 
- def __init__(self, x, y, z, larg, long, haut):
+    def __init__(self, x, y, z, larg, long, haut):
         """Constructeur de la classe Mur"""
         Cube.__init__(self,x,y,z,larg,long,haut)
 
@@ -17,10 +19,20 @@ class Mur(Cube):
         print("Mur(x=%.2f,y=%.2f,z=%.2f, larg=%.2f,long=%.2f,haut=%.2f)"%(self.x, self.y, self.z, self.larg, self.long, self.haut))
 
 
-def Creation_Mur(x,y,z,long,arene):     #ajouter des parametres c'est étrange pour une création rapide de mur mais bon pourquoi pas :)
+def Creation_Mur(arene):
     """Création d'un mur avec une hauteur et une epaisseur fixé par les limites de l'Arene"""
+
+    x = random.randint(0, arene.lx)
+    y = random.randint(0, arene.ly)
+    z = 1   #un mur est posé au sol
+
+    larg = random.randint(10, 50) #largeur arbitraire
+    long = random.randint(20,100)
+    haut = arene.lz-1   #un mur va jusq'au plafond 
     
-    return Mur(x, y, z, 10, long, arene.lz)
+    return Mur(x, y, z, larg, long, haut)
+
+
 
 #creation d'u constructeur temporaire pour l'affichage tkinter
 def Creation_Mur_xy(x, y):
@@ -35,5 +47,4 @@ def Creation_Mur_xy(x, y):
     haut = 499  # un mur monte jusqu'au plafond
 
     return Mur(x, y, z, larg, long, haut)
-    
     
