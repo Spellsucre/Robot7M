aa43b8c8c5884d7df7eada7d8a1b570e80a7d97c
ryan o <rohouens@yahoo.fr>
2018-02-15 10:44:17 +0100

    fix des bugs liee aux precedents commit

--- Simulation/ModelePhysique.py
+++ Simulation/ModelePhysique.py
@@ -32,6 +32,4 @@ with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
 		listener.join()
 	except MyException as e:
 		print('{0} was pressed'.format(e.args[0]))
-
-print('a')
 			
